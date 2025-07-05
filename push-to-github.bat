a63cc2232c43b3992ce9d533c9e70d944508208f
Irfan Master <sales@high5clothing.co.uk>
2025-07-05 01:34:31 +0100

    new dash

--- push-to-github.bat
+++ push-to-github.bat
@@ -3,6 +3,7 @@ cd /d "%~dp0"
 echo --------------------------------------------------
 echo Starting Git Push for dashboard-app
 echo --------------------------------------------------
+git pull --rebase origin main
 git add .
 set /p msg="Enter commit message: "
 git commit -m "%msg%"
