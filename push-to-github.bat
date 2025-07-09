21f7a45d48a52e1804ff821ddbce3b010c93697e
iffy4ever <sales@high5clothing.co.uk>
2025-07-09 23:14:40 +0100

    Auto-update: 09/07/2025-23:14:40.60

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,12 +1,22 @@
 @echo off
 cd /d "%~dp0"
 
-:: Ensure HTTPS URL
+:: 1. Force-update remote URL (HTTPS)
 git remote set-url origin https://github.com/iffy4ever/high5-dashboard.git
 
-:: Standard workflow
-git add .
-git commit -m "Update: %date% %time%"
-git push origin master
+:: 2. Add ALL changes (including deleted files)
+git add --all
 
+:: 3. Create commit with timestamp
+set timestamp=%date%-%time%
+git commit -m "Auto-update: %timestamp%"
+
+:: 4. Force-push to master (overwrite remote if needed)
+git push --force origin master
+
+:: 5. Verify
+git log --oneline -3
+git status
+
+echo Update COMPLETE - Check GitHub now
 pause
