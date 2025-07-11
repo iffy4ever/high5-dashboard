c3db0eae5a82cfecac6a558ccc649758fc368e75
Irfan Master <sales@high5clothing.co.uk>
2025-07-12 00:35:42 +0100

    Auto-update: 12/07/2025  0:35:42.31

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,11 +1,18 @@
 @echo off
+:: Batch file to push updates to GitHub
+:: Save this as 'push-to-github.bat' in your project folder
+
+echo Starting Git operations...
+
+:: Navigate to your project directory (if running from elsewhere)
 cd /d "%~dp0"
-echo --------------------------------------------------
-echo Starting Git Push for dashboard-app
-echo --------------------------------------------------
-git pull --rebase origin main
+
+:: Git commands
 git add .
-set /p msg="Enter commit message: "
-git commit -m "%msg%"
-git push
+git commit -m "Auto-update: %date% %time%"
+git push origin main
+
+echo.
+echo Git push completed!
+echo Repository: https://github.com/iffy4ever/high5-dashboard
 pause
