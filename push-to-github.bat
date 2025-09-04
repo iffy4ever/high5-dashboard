e049c6c88f612af1daa97b1dfe953aa11d95ccb0
iffy4ever <sales@high5clothing.co.uk>
2025-09-04 10:32:22 +0100

    Auto update - 04/09/2025 10:32:22.04

--- push-to-github.bat
+++ push-to-github.bat
@@ -8,5 +8,18 @@ git commit -m "Auto update - %date% %time%"
 for /f "delims=" %%i in ('git branch --show-current') do set BRANCH=%%i
 git push origin %BRANCH%
 
-echo Update complete!
+if %errorlevel% equ 0 (
+    echo.
+    echo Update successful!
+    echo.
+    echo NEXT STEPS:
+    echo 1. Go to your GitHub repository:
+    echo    https://github.com/iffy4ever/high5-dashboard
+    echo 2. Click 'Compare & pull request' button
+    echo 3. Review changes and create pull request
+    echo 4. Merge the pull request to complete update
+) else (
+    echo Error during push!
+)
+
 pause
